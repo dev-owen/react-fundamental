b2aa0ec81826e29287644107e7af3cc62d023952
dev-owen <jgyuity1289@gmail.com>
2023-05-10 00:15:19 +0900

    practice/304 : form input starter

---
+++ src/components/NewPayment/PaymentForm.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import './PaymentForm.css';
+
+const PaymentForm = () => {
+  return (
+    <form>
+      <div className='new-payment__controls'>
+        <div className='new-payment__control'>
+          <label>Title</label>
+          <input type='text' />
+        </div>
+        <div className='new-payment__control'>
+          <label>Amount</label>
+          <input type='number' min='0.01' step='0.01' />
+        </div>
+        <div className='new-payment__control'>
+          <label>Date</label>
+          <input type='date' min='2019-01-01' max='2022-12-31' />
+        </div>
+      </div>
+      <div className='new-payment__actions'>
+        <button type='submit'>Add Expense</button>
+      </div>
+    </form>
+  );
+};
+
+export default PaymentForm;
