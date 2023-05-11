8fbe403fc1b55e1e8ed16c99d1fa9113b1e88fec
dev-owen <jgyuity1289@gmail.com>
2023-05-12 03:23:17 +0900

    fix

--- src/components/Payments/Expenses.js
+++ src/components/Payments/Expenses.js
@@ -7,11 +7,13 @@ import './Expenses.css';
 const Expenses = (props) => {
   return (
     <Card className="expenses">
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
+      {props.items.map(item => (
+        <ExpenseItem
+          title={item.title}
+          amount={item.amount}
+          date={item.date}
+        />
+      ))}
     </Card>
   );
 }
