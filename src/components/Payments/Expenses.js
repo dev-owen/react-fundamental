a8229808d444500a0c124c561767c51e95e8a9ce
dev-owen <jgyuity1289@gmail.com>
2023-05-12 03:04:27 +0900

    practice/3-6: starter

---
+++ src/components/Payments/Expenses.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import ExpenseItem from './ExpenseItem';
+import Card from '../UI/Card';
+import './Expenses.css';
+
+const Expenses = (props) => {
+  return (
+    <Card className="expenses">
+      <ExpenseItem
+        title={props.items[0].title}
+        amount={props.items[0].amount}
+        date={props.items[0].date}
+      />
+    </Card>
+  );
+}
+
+export default Expenses;
