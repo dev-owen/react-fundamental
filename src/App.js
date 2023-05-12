4a8ce2b34a64c9d95c44fcc24d4fa3fad61d63fb
dev-owen <jgyuity1289@gmail.com>
2023-05-12 23:49:17 +0900

    practice/3-7: starter

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
-import './App.css';
-import NewPayment from './components/NewPayment/NewPayment';
-import Expenses from './components/Payments/Expenses';
-
 function App() {
-  const expenses = [
-    {
-      id: 'e1',
-      title: '수건',
-      amount: 12.33,
-      date: new Date(2025, 8, 14),
-    }
-  ];
-
   return (
-    <>
-      <NewPayment/>
-      <Expenses items={expenses} />
-    </>
+    <div>
+      <p>hello image</p>
+    </div>
   );
 }
 
