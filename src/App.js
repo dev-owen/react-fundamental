65f25fb62c9a22f5ffb99b42409cb73af0d00872
dev-owen <jgyuity1289@gmail.com>
2023-05-18 21:44:24 +0900

    fix

--- src/App.js
+++ src/App.js
@@ -9,12 +9,35 @@ function App() {
 			id: "e1",
 			title: "수건",
 			amount: 12.33,
-			date: new Date(2025, 8, 14),
+			date: new Date(2022, 3, 14),
+		},
+		{
+			id: "e2",
+			title: "물티슈",
+			amount: 234.11,
+			date: new Date(2023, 8, 22),
+		},
+		{
+			id: "e3",
+			title: "모니터",
+			amount: 3331.33,
+			date: new Date(2021, 10, 22),
+		},
+		{
+			id: "e4",
+			title: "의자",
+			amount: 298,
+			date: new Date(2022, 1, 31),
+		},
+		{
+			id: "e4",
+			title: "의자",
+			amount: 298,
+			date: new Date(2021, 6, 1),
 		},
 	]);
 
 	const getPaymentFormData = (data) => {
-		console.log(data);
 		setExpenses([
 			{
 				id: Math.random().toString(),
