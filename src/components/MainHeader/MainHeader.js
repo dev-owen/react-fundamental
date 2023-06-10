b80eeb54040a0a26ecd9e90bfe88a52f663e754c
dev-owen <jgyuity1289@gmail.com>
2023-06-11 01:04:25 +0900

    practice/4-9: starter

--- src/components/MainHeader/MainHeader.js
+++ src/components/MainHeader/MainHeader.js
@@ -7,7 +7,7 @@ const MainHeader = (props) => {
   return (
     <header className={classes['main-header']}>
       <h1>리액트 페이지</h1>
-      <Navigation isLoggedIn={props.isAuthenticated} onLogout={props.onLogout} />
+      <Navigation />
     </header>
   );
 };
