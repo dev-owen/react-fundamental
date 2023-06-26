c690ed76214281c74d952da43b39bb0d96bc2a3d
dev-owen <jgyuity1289@gmail.com>
2023-06-26 21:58:48 +0900

    fix: text edit

--- src/components/Tasks/Tasks.js
+++ src/components/Tasks/Tasks.js
@@ -1,35 +1,35 @@
-import Section from '../UI/Section';
-import TaskItem from './TaskItem';
-import classes from './Tasks.module.css';
+import Section from "../UI/Section";
+import TaskItem from "./TaskItem";
+import classes from "./Tasks.module.css";
 
 const Tasks = (props) => {
-  let taskList = <h2>No tasks found. Start adding some!</h2>;
+	let taskList = <h2>태스크가 없습니다. 추가해주세요.</h2>;
 
-  if (props.items.length > 0) {
-    taskList = (
-      <ul>
-        {props.items.map((task) => (
-          <TaskItem key={task.id}>{task.text}</TaskItem>
-        ))}
-      </ul>
-    );
-  }
+	if (props.items.length > 0) {
+		taskList = (
+			<ul>
+				{props.items.map((task) => (
+					<TaskItem key={task.id}>{task.text}</TaskItem>
+				))}
+			</ul>
+		);
+	}
 
-  let content = taskList;
+	let content = taskList;
 
-  if (props.error) {
-    content = <button onClick={props.onFetch}>Try again</button>;
-  }
+	if (props.error) {
+		content = <button onClick={props.onFetch}>다시 시도해주세요</button>;
+	}
 
-  if (props.loading) {
-    content = 'Loading tasks...';
-  }
+	if (props.loading) {
+		content = "태스크 로딩중...";
+	}
 
-  return (
-    <Section>
-      <div className={classes.container}>{content}</div>
-    </Section>
-  );
+	return (
+		<Section>
+			<div className={classes.container}>{content}</div>
+		</Section>
+	);
 };
 
 export default Tasks;
