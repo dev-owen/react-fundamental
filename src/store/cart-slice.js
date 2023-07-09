18ada55863959a956d2f66200c7a9f502077ad23
dev-owen <jgyuity1289@gmail.com>
2023-07-09 09:08:49 +0900

    practice/5_8: starter(updated)

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -7,33 +7,8 @@ const cartSlice = createSlice({
     totalQuantity: 0,
   },
   reducers: {
-    addItemToCart(state, action) {
-      const newItem = action.payload;
-      const existingItem = state.items.find((item) => item.id === newItem.id);
-      state.totalQuantity++;
-      if (!existingItem) {
-        state.items.push({
-          id: newItem.id,
-          price: newItem.price,
-          quantity: 1,
-          totalPrice: newItem.price,
-          name: newItem.title
-        });
-      } else {
-        existingItem.quantity++;
-        existingItem.totalPrice = existingItem.totalPrice + newItem.price;
-      }
-    },
-    removeItemFromCart(state, action) {
-      const id = action.payload;
-      const existingItem = state.items.find(item => item.id === id);
-      state.totalQuantity--;
-      if (existingItem.quantity === 1) {
-        state.items = state.items.filter(item => item.id !== id);
-      } else {
-        existingItem.quantity--;
-      }
-    },
+    addItemToCart(state, action) { /*TODO- 여기에 코드를 작성해 주세요*/},
+    removeItemFromCart(state, action) { /*TODO- 여기에 코드를 작성해 주세요*/},
   },
 });
 
